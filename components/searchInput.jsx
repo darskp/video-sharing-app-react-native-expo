fa43ff1d0761dd4c4cb8e25693d936fe983a0066
darskp <darshanpatgar100@gmail.com>
2024-04-21 16:40:51 +0530

    search screen

--- components/searchInput.jsx
+++ components/searchInput.jsx
@@ -1,20 +1,32 @@
-import { View, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { icons } from '../constants'
+import { router, usePathname } from 'expo-router'
 
-export default function SearchInput({ title, value, handleChangeText, otherStyles, placeholder, keyboardType, ...props }) {
-    const [showPassword, setShowPassword] = useState(false)
+export default function SearchInput({ initialQuery }) {
+    const pathName = usePathname();
+    const [query, setquery] = useState(initialQuery || '')
+
     return (
         <View className=" space-x-4 border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
             <TextInput
                 className="text-base mt-0.5 flex-1 text-white font-pregular"
-                value={value}
+                value={query}
                 placeholder="Search for a video topic"
-                onChangeText={handleChangeText}
-                placeholderTextColor="#7b7b8b"
-                secureTextEntry={title == 'Password' && !showPassword}
+                onChangeText={(e) => setquery(e)}
+                placeholderTextColor="#CDCDe0"
             />
-            <TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    if (!query) {
+                        return Alert.alert('Missing Query', "Please input something to search results across database")
+                    }
+                    if (pathName.startsWith('/search'))
+                        router.setParams({ query })
+                    else
+                        router.push(`/search/${query}`)
+                }}
+            >
                 <Image
                     source={icons.search}
                     resizeMode='contain'
